6a5cb00867688c43d8a37d773404ddada4c0a11a
avinashparsad906 <avinash.parsad@osganalytics.com>
2021-11-26 23:40:46 +0530

    Initial commit

---
+++ src/main/java/com/asquare/demo/AppWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.asquare.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppWebservicesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/asquare/demo/AppWebservicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asquare.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppWebservicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
